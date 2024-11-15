bdfd64ce7769d1dd2240ac7ce5fce1fa556716c1
roger rodriguez <rrodriguezticuna@gmail.com>
2024-11-14 21:06:29 -0500

    inicios de python

---
+++ dia_1/1_variables.py
@@ -0,0 +1,20 @@
+# Tipos de datos de variables primitivas
+
+# String
+nombre = "Roger'Rodriguez"
+dirección = 'Las Gardenias "523'
+
+# Cuando queremos guardar en texto muy largo que ademas que puede tener saltos de linea
+enunciado = ''' Hola como estas
+Te extraño, 
+espero verte pronto BB'''
+
+otro_enucniado = """Hola
+como estas BB
+Te quiero Muchismo bbcta,
+Saludos mi Vida"""
+
+#Int
+edad = 34
+# Boolean 
+
